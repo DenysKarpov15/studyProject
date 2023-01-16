393254b80de056aa6b0918699e557fcb98b574f6
Denys Karpov <el.denis157@gmail.com>
2023-01-16 15:52:53 +0200

    Homework for lesson 2 done!

---
+++ src/com/ua/robot/homework2/Main.java
@@ -0,0 +1,10 @@
+package com.ua.robot.homework2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+        System.out.println("This is my first homework for lesson 2");
+    }
+
+}
