d8b1010b0999c1b5cd24918808d09ee411729e22
Denys Karpov <el.denis157@gmail.com>
2023-03-30 13:15:36 +0300

    Homework for lesson 22

---
+++ src/com/ua/robot/homework22/LastNameComparator.java
@@ -0,0 +1,11 @@
+package com.ua.robot.homework22;
+
+import java.util.Comparator;
+
+public class LastNameComparator implements Comparator<Student> {
+
+    public int compare(Student s1, Student s2) {
+        return s1.getLastName().compareTo(s2.getLastName());
+
+    }
+}
