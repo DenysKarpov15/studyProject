18f61bc522d44a949d0736c37002a2466861641b
Denys Karpov <el.denis157@gmail.com>
2023-04-22 14:19:01 +0300

    Homework for lesson 28

--- src/com/ua/robot/homework28/FileHandlerNIO.java
+++ src/com/ua/robot/homework28/FileHandlerNIO.java
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class FileHandler {
+public class FileHandlerNIO {
 
     public void saveToFile(String text, String filename) {
         try {
--- src/com/ua/robot/homework28/Main.java
+++ src/com/ua/robot/homework28/Main.java
@@ -1,7 +1,5 @@
 package com.ua.robot.homework28;
 
-import com.ua.robot.homework27.FileHandler;
-
 public class Main {
 
     public static final String FILE_NAME = "new_text.txt";
@@ -9,8 +7,9 @@ public class Main {
     public static void main(String[] args) {
 
         String text = "This is file created using NIO library!";
-        FileHandler.saveToFile(text, FILE_NAME);
-        String data = FileHandler.readFromFile(FILE_NAME);
+        FileHandlerNIO handler = new FileHandlerNIO();
+        handler.saveToFile(text, FILE_NAME);
+        String data = handler.readFromFile(FILE_NAME);
         System.out.println(data);
     }
 }
