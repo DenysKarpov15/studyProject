996fbce5f5e32615b8a0b255e0f8ca26543ea829
Denys Karpov <el.denis157@gmail.com>
2023-05-01 16:43:57 +0300

    Homework for lesson 33

---
+++ src/com/ua/robot/homework33/Main.java
@@ -0,0 +1,10 @@
+package com.ua.robot.homework33;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        NumberGeneratorThread thread = new NumberGeneratorThread();
+        thread.start();
+    }
+}
